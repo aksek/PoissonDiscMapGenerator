7040f525a07a3068a5a37779e3702c81fe3740cc
aksek <kosekaniela@gmail.com>
2020-05-31 19:41:44 +0200

    the algorithm now starts after pressing the button

--- src/main/java/MapGenerator/GUI.java
+++ src/main/java/MapGenerator/GUI.java
@@ -2,12 +2,14 @@ package MapGenerator;
 
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Slider;
+import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.event.EventHandler;
+import javafx.event.ActionEvent;
 
 public class GUI {
     public void initUI(Stage stage) {
@@ -15,8 +17,6 @@ public class GUI {
         Pane display = new Pane();
         display.setMinSize(400, 500);
 
-        Text lblVertices = new Text("Vertices: ");
-        Slider sldVertices = new Slider(1.0, 0.0, 100.0);
 
         GridPane menu = new GridPane();
         menu.setMinSize(500, 500);
@@ -24,8 +24,36 @@ public class GUI {
         menu.setVgap(5);
         menu.setHgap(5);
 
-        menu.add(lblVertices, 0, 0);
-        menu.add(sldVertices, 1, 0);
+        Text lblMinDist = new Text("Min distance: ");
+        Spinner spnMindDist = new Spinner(1, 100, 1);
+
+        Text lblSpeed = new Text("Simulation speed: ");
+        Spinner spnSpeed = new Spinner(1, 100, 1);
+
+        final ToggleGroup mode = new ToggleGroup();
+
+        RadioButton rbPoisson = new RadioButton("Poisson distribution");
+        rbPoisson.setToggleGroup(mode);
+        RadioButton rbRandom = new RadioButton("Random distribution");
+        rbRandom.setToggleGroup(mode);
+
+        Button btnStart = new Button("Start!");
+
+        EventHandler<ActionEvent> btnStartPressed = new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent e) {
+                PoissonDisc poisson = new PoissonDisc(100, 10, 500, 400, display);
+            }
+        };
+        btnStart.setOnAction(btnStartPressed);
+
+        menu.add(lblMinDist, 0, 1);
+        menu.add(spnMindDist, 1, 1);
+        menu.add(lblSpeed, 0, 2);
+        menu.add(spnSpeed, 1, 2);
+        menu.add(rbPoisson, 0, 3);
+        menu.add(rbRandom, 1, 3);
+        menu.add(btnStart, 1, 4);
+
 
         GridPane root = new GridPane();
         root.add(display, 0, 0);
@@ -37,6 +65,6 @@ public class GUI {
         stage.setScene(scene);
         stage.show();
 //        randomDistribution rand = new randomDistribution(display);
-        PoissonDisc poisson = new PoissonDisc(100, 10, 500, 400, display);
+
     }
 }
