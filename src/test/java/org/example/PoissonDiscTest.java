20863f1f8e0312f2630944efa69b7d01f20ca345
aksek <kosekaniela@gmail.com>
2020-06-10 17:00:28 +0200

    corrected tests (used a better suited function)

--- src/test/java/org/example/PoissonDiscTest.java
+++ src/test/java/org/example/PoissonDiscTest.java
@@ -12,13 +12,13 @@ class PoissonDiscTest {
     void checkCandidate() {
         PoissonDisc tester = new PoissonDisc(10, 10,500, 400);
         tester.addVertex(new Point(10, 10));
-        Assertions.assertEquals(false, tester.checkCandidate(new Point(10, 15)));
-        Assertions.assertEquals(false, tester.checkCandidate(new Point(12, 12)));
-        Assertions.assertEquals(false, tester.checkCandidate(new Point(10, 8)));
-        Assertions.assertEquals(false, tester.checkCandidate(new Point(19, 10)));
-        Assertions.assertEquals(false, tester.checkCandidate(new Point(8, 10)));
-        Assertions.assertEquals(false, tester.checkCandidate(new Point(8, 8)));
-        Assertions.assertEquals(true, tester.checkCandidate(new Point(21, 0)));
+        Assertions.assertFalse(tester.checkCandidate(new Point(10, 15)));
+        Assertions.assertFalse(tester.checkCandidate(new Point(12, 12)));
+        Assertions.assertFalse(tester.checkCandidate(new Point(10, 8)));
+        Assertions.assertFalse(tester.checkCandidate(new Point(19, 10)));
+        Assertions.assertFalse(tester.checkCandidate(new Point(8, 10)));
+        Assertions.assertFalse(tester.checkCandidate(new Point(8, 8)));
+        Assertions.assertTrue(tester.checkCandidate(new Point(21, 0)));
     }
 
     @Test
@@ -27,7 +27,7 @@ class PoissonDiscTest {
         Point current = new Point(10, 10);
         tester.addVertex(current);
         Point candidate = tester.getNextCandidate(current);
-        Assertions.assertEquals(true, Math.pow(candidate.x - 10, 2) + Math.pow(candidate.y - 10, 2) >= 100);
-        Assertions.assertEquals(true, Math.pow(candidate.x - 10, 2) + Math.pow(candidate.y - 10, 2) <= 400);
+        Assertions.assertTrue(Math.pow(candidate.x - 10, 2) + Math.pow(candidate.y - 10, 2) >= 100);
+        Assertions.assertTrue(Math.pow(candidate.x - 10, 2) + Math.pow(candidate.y - 10, 2) <= 400);
     }
 }
