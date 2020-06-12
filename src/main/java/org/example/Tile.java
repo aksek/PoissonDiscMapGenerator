3c32d3216d859269f5a0067f268f1f10fdc96178
aksek <kosekaniela@gmail.com>
2020-06-12 11:56:17 +0200

    create a test for createSuperTriangle()

--- src/main/java/org/example/Tile.java
+++ src/main/java/org/example/Tile.java
@@ -1,34 +1,25 @@
 package org.example;
 
-public class Tile {
-    int X_a, Y_a;
-    int X_b, Y_b;
-    int X_c, Y_c;
+import java.awt.*;
 
-    public Tile(int Xa, int Ya, int Xb, int Yb, int Xc, int Yc) {
-        X_a = Xa;
-        Y_a = Ya;
-        X_b = Xb;
-        Y_b = Yb;
-        X_c = Xc;
-        Y_c = Yc;
+public class Tile {
+    private Point A;
+    private Point B;
+    private Point C;
+
+    public Tile(Point a, Point b, Point c) {
+        A = a;
+        B = b;
+        C = c;
     }
-    public int Xa() {
-        return X_a;
-    }
-    public int Ya() {
-        return Y_a;
-    }
-    public int Xb() {
-        return X_b;
-    }
-    public int Yb() {
-        return Y_b;
-    }
-    public int Xc() {
-        return X_c;
-    }
-    public int Yc() {
-        return Y_c;
-    }
+    public int Xa() { return A.x; }
+    public int Ya() { return A.y; }
+    public int Xb() { return B.x; }
+    public int Yb() { return B.y; }
+    public int Xc() { return C.x; }
+    public int Yc() { return C.y; }
+
+    public Point A() { return A; }
+    public Point B() { return B; }
+    public Point C() { return C; }
 }
--- src/test/java/org/example/TriangulationTest.java
+++ src/test/java/org/example/TriangulationTest.java
@@ -1,19 +1,43 @@
 package org.example;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
+import java.awt.*;
+import java.util.Vector;
+
 public class TriangulationTest {
+    @Disabled
     @Test
     void createSuperTriangle() {
-//        Triangulation tester = new Triangulation()
+        Vector<Point> vertices = new Vector<>();
+        vertices.addElement(new Point(1, 1));
+        vertices.addElement(new Point(5, 1));
+        vertices.addElement(new Point(1, 10));
+        vertices.addElement(new Point(5, 10));
+        Triangulation tester = new Triangulation(Vector<Point> vertices);
+        Tile superTriangle = tester.createSuperTriangle();
+        for (Point vertex : vertices) {
+            Assertions.assertTrue(onSameSideOfLine(vertex, superTriangle.A(), superTriangle.B(), superTriangle.C()));
+            Assertions.assertTrue(onSameSideOfLine(vertex, superTriangle.B(), superTriangle.C(), superTriangle.A()));
+            Assertions.assertTrue(onSameSideOfLine(vertex, superTriangle.C(), superTriangle.A(), superTriangle.B()));
+        }
+
+    }
+    boolean onSameSideOfLine(Point p1, Point p2, Point lineThrough1, Point lineThrough2) {
+        double a = (double)(lineThrough2.y - lineThrough1.y)/(lineThrough2.x - lineThrough1.x);
+        double b = lineThrough1.y - a * lineThrough1.x;
+        if ((p1.y < a * p1.x + b) == (p2.y < a * p2.x + b))
+            return true;
+        else return false;
     }
     @Test
     void getInvalidTriangles() {
 
     }
     @Test
-    void getPolygonHole() {
+    void getPolygonVertices() {
 
     }
 }
